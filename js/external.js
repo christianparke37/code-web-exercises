2135f73cb80689b74bd2af702591608499e21845
Chris parker <christianparke37@gmail.com>
2020-08-10 17:00:44 -0500

    mostly done files

--- js/external.js
+++ js/external.js
@@ -1,15 +1,25 @@
-console.log('Hello from JavaScript!');
-console.log("Hello from JSwithHTML.js external file");
+console.log("Hello from external JavaSecript");
+
+alert("Welcome to Website!")
+
+var question = "What is your favorite color?";
+var response = prompt(question);
+alert ("Great, " + response + " is my favorite color, too!");
+
+var littleMermaidDays = prompt("How many days did you rent the Little Mermaid?");
+var brotherBrother = prompt("How many days did you rent the Brother Bear?");
+var hercules = prompt("How many days did you rent the Hercules?");
+var blockBusterBill = (+littleMermaidDays + +brotherBearDays + +herculesDays) *3;
+alert(Total Blockbuster Bill$ " + blockBusterBill");
 
 
-var suprise = "Hello!!!!!";
-console.log(suprise);
+var googleRate: 400;
+var amazonRate: 380;
+var facebookRate: 350;
+var googlehours = prompt("How many hours did you work at Google?");
+var facebookhours = prompt("How many hours did you work at Facebook?");
+var amazonhours = prompt("How many hours did you work at Amazon?");
 
-console.log("Continuing on...")
-alert(suprise);
+var salary = (+googlehours * googleRate) + (+facebookhours * facebookRate) + (+amazonhours * amazonRate)
 
-var color3 = "What is your favorite color?"
-var color3 = prompt(color3);
-alert("Great! Blue is my favorite color, too!!!")
-console.log(color3);
-console.log(typeof color3)
+console.log("You made $" + salary + " this week!");
