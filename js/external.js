38ab63f9f9f15943853167c50a5a2818c4b9482e
Chris parker <christianparke37@gmail.com>
2020-08-10 15:38:34 -0500

    JavaScript changes

---
+++ js/external.js
@@ -0,0 +1,15 @@
+console.log('Hello from JavaScript!');
+console.log("Hello from JSwithHTML.js external file");
+
+
+var suprise = "Hello!!!!!";
+console.log(suprise);
+
+console.log("Continuing on...")
+alert(suprise);
+
+var color3 = "What is your favorite color?"
+var color3 = prompt(color3);
+alert("Great! Blue is my favorite color, too!!!")
+console.log(color3);
+console.log(typeof color3)
