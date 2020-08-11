45704b3c1eb61453034d79de6c87f0717eaa6af4
Chris parker <christianparke37@gmail.com>
2020-08-11 10:02:25 -0500

    Additional changes

--- js/external.js
+++ js/external.js
@@ -20,6 +20,21 @@ var googlehours = prompt("How many hours did you work at Google?");
 var facebookhours = prompt("How many hours did you work at Facebook?");
 var amazonhours = prompt("How many hours did you work at Amazon?");
 
-var salary = (+googlehours * googleRate) + (+facebookhours * facebookRate) + (+amazonhours * amazonRate)
+console.log = (+googlehours * googleRate) + (+facebookhours * facebookRate) + (+amazonhours * amazonRate)
 
-console.log("You made $" + salary + " this week!");
+console.log("You made $" + salary.toFixed(2) + " this week!");
+
+
+var classCapacity = 30;
+var registeredStudents = 20;
+var classDoesNotConflict = confirm("Press okay if ths does not conflict with your schedule.");
+var canEnroll = (classCapacity > registeredStudents) && !classConflicts;
+
+alert("Can she register?" + canEnroll);
+
+// A product offer can be applied only if a perso more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+var offerNOTExpired = confirm("Press okay if offer not expired.");
+var numberItems = prompt("How many items are you purchasing?");
+var premiumMembership = confirm("Press okay if you are a premium member.");
+var offerApplied = (numberItems > 2 || premiumMembership) &&!offerNotExpired;
+console.log("Offer applied " + offerApplied);
