70fe1292b7d7006c783da49a27ee2d860e903ffc
Chris parker <christianparke37@gmail.com>
2020-08-11 08:40:16 -0500

    added changes

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
-console.log("Hello from external JavaSecript");
+console.log("Hello from external JavaScript");
 
 alert("Welcome to Website!")
 
@@ -7,15 +7,15 @@ var response = prompt(question);
 alert ("Great, " + response + " is my favorite color, too!");
 
 var littleMermaidDays = prompt("How many days did you rent the Little Mermaid?");
-var brotherBrother = prompt("How many days did you rent the Brother Bear?");
-var hercules = prompt("How many days did you rent the Hercules?");
+var brotherBearDays = prompt("How many days did you rent the Brother Bear?");
+var herculesDays = prompt("How many days did you rent the Hercules?");
 var blockBusterBill = (+littleMermaidDays + +brotherBearDays + +herculesDays) *3;
-alert(Total Blockbuster Bill$ " + blockBusterBill");
+alert("Your total Blockbuster Bill is $ " + blockBusterBill);
 
 
-var googleRate: 400;
-var amazonRate: 380;
-var facebookRate: 350;
+var googleRate = 400;
+var amazonRate = 380;
+var facebookRate = 350;
 var googlehours = prompt("How many hours did you work at Google?");
 var facebookhours = prompt("How many hours did you work at Facebook?");
 var amazonhours = prompt("How many hours did you work at Amazon?");
