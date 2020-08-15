82b1d39b268179fc50f6ccc10309e86877722914
Chris parker <christianparke37@gmail.com>
2020-08-15 09:31:39 -0500

    Javascript practice

--- js/javascriptpractice.js
+++ js/javascriptpractice.js
@@ -6,3 +6,56 @@ const division = 100 / 2
 const firstName = "Luke"
 const lastName = "Skywalker"
 var name = firstName + lastName
+function multiplyTwoNumbers(num1, num2) {
+    return num1 * num2
+}
+function hello () {
+    return "Hello World!"
+}
+const fruit = ["apple", "banana", "orange", "mango", "tomato"]
+
+const stuff = [true, 1976, null, "hey"]
+fruit [2]
+const numbers = [ 1, 4, 6, 8, 0]
+numbers [1] = 88
+
+const movies = ["The Matrix", "Se7en", "The Wizard of Oz", "Mary Poppins"]
+movies.length
+// The return would be four, because there are four elements in the array
+
+const firstNames = ["Tyrion", "Jon", "Jorah", "Arya", Joffrey"]
+const lastNames = ["Lannister", "Snow", "Mormont", "Stark", "Baratheon"]
+
+const person1 = {
+    firstName: "Joffrey",
+    lastName: "Baratheon",
+    email: "joff@widowsail.com"
+}
+
+const person2 = {
+    firstName: "Jon",
+    lastName: "Snow",
+    email: "brooding@thewall.com"
+}
+
+const person3 = {
+    firstName: "Tyrion",
+
+}
+
+const person4 = {
+    firstName: "Daenerys",
+    lastName: "Targaryen",
+    email: "dragonlady@gmail.com".
+}
+
+function getEmail(person) {
+    return person.email
+
+}
+
+const numbers = [1, 4, 9, 16]
+
+function subtractTwo(numbers) {
+    return numbers.map(x => x - 2)
+}
