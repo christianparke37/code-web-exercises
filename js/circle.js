438605a50b779c73e05372836633ce21559685ba
Chris parker <christianparke37@gmail.com>
2020-08-25 08:42:33 -0500

    Circle.js

--- js/circle.js
+++ js/circle.js
@@ -8,22 +8,21 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-            return Math.PI * Math.pow(this.radius, 2);
-            // return Math.PI * (this.radius * this.radius); // TODO: return the proper value
-            console.log("The area of this circle is" + area);
+            return (Math.PI * (Math.pow(this.radius, 2)))
+            // TODO: return the proper value
         },
-
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-            this.logInfo = Math.round(this.getArea())
+            if (doRounding === true) {
+                console.log(Math.round(this.getArea()))
+            } else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            }
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-            if (this.getArea() % 2 === 1) {
-                return this.logInfo;
-            }
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+
 
         }
     };
@@ -36,7 +35,6 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
